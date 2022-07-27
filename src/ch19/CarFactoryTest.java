be2b57cfdc97a9b5454e134198a82b4e748fbf1b
G <G@DESKTOP-1FFOQKL>
2022-07-27 17:46:35 +0900

    .

---
+++ src/ch19/CarFactoryTest.java
@@ -0,0 +1,48 @@
+package ch19;
+
+class Car {
+	private static int serialNum = 1000;
+	private int carNum;
+	
+	public Car() {
+		serialNum++;
+		carNum = serialNum;
+	}
+
+	public int getCarNum() {
+		return carNum;
+	}
+
+	public void setCarNum(int carNum) {
+		this.carNum = carNum;
+	}
+}
+
+class CarFactory {
+	private static CarFactory instance = new CarFactory();
+	
+	private CarFactory() {}
+	
+	public static CarFactory getInstance() {
+		if(instance == null) 
+			instance = new CarFactory();
+		return instance;
+	}
+	
+	public Car createCar() {
+		Car car = new Car();
+		return car;
+	}
+}
+
+public class CarFactoryTest {
+	public static void main(String[] args) {
+		
+		CarFactory factory = CarFactory.getInstance();
+		Car mySonata = factory.createCar();
+		Car yourSonata = factory.createCar();
+		
+		System.out.println(mySonata.getCarNum());
+		System.out.println(yourSonata.getCarNum());
+	}
+}
